6800ce67628c8e44a6c71d5670d596e99bd6e35e
Casper <vishal.r.dev07@gmail.com>
2025-12-25 10:56:05 +0530

    changed to classes

--- pdfExtracter.py
+++ pdfExtracter.py
@@ -1,17 +1,26 @@
 import os
 import PyPDF2
 from tagExtracter import tagExtracter
+class extractPDF:
+    def __init__(self, pdf_path):
+        self.pdf_path = pdf_path
+        self.extracter = tagExtracter(api_key = os.getenv("GEMINI_API_KEY"), model = "gemini-2.5-flash")
+    
+    def extract_text(self):
+        with open(self.pdf_path, 'rb') as file:
+            reader = PyPDF2.PdfReader(file)
+            text = ""
+            for page in reader.pages:
+                text += page.extract_text()
+        return text
 
-def extract_text_from_pdf(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        text = ""
-        for page in reader.pages:
-            text += page.extract_text()
-    return text
+    def extract_tags(self):
+        resume_text = self.extract_text()
+        tags = self.extracter.extract(resume_text)
+        return tags
 
-file = r""
-resume_text = extract_text_from_pdf(file)
-extracter = tagExtracter(api_key = os.getenv("GEMINI_API_KEY"), model = "gemini-2.5-flash")
-tags = extracter.extract(resume_text)
-print(tags)
+if __name__ == "__main__":
+    pdf_path = ""
+    pdf = extractPDF(pdf_path)
+    tags = pdf.extract_tags()
+    print(tags)
